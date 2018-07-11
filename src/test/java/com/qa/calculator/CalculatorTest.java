2474a87cbb1d15b1e42ce3a12e593970ff81eb5b
D1efe <d.osagie95@hotmail.co.uk>
2018-07-11 11:38:14 +0100

    test for addition written to fail

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.qa.calculator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -8,21 +10,35 @@ import com.qa.subtraction.Subtraction;
 
 public class CalculatorTest {
 
-	private static Subtraction subtraction;
-	
-	@BeforeClass
-	public static void setUp() {
+	private Subtraction subtraction;
+	private Addition addition;
+
+	@Before
+	public void setUp() {
 		subtraction = new Subtraction();
 	}
 
 	@Test
 	public void subtractTest() {
 
-		
 		int actualResult = subtraction.subtract(9, 4);
-		
+
 		assertEquals(5, actualResult);
-		
+
+	}
+
+	@Before
+	public void additionSetUp() {
+		addition = new Addition();
+	}
+
+	@Test
+	public void addTest() {
+
+		int actualResult = addition.add(13, 7);
+
+		assertEquals(20, actualResult);
+
 	}
 
 }
